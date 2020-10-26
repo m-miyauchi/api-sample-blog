a5087c30214a3004b1bb96973e9bc85824bd5077
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-26 15:24:46 +0900

    f

--- src/app.ts
+++ src/app.ts
@@ -7,34 +7,29 @@ const app: Express = express();
 app.use(morgan('dev'));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('API for sample blog app.');
 });
 
 // ログイン
 app.put('/login', (req: Request, res: Response) => {
   res.send('');
 });
-
 // ログアウト
 app.put('/logout', (req: Request, res: Response) => {
   res.send('');
 });
-
 // 記事一覧取得
 app.get('/post', (req: Request, res: Response) => {
   res.send('');
 });
-
 // 記事詳細取得
 app.get('/post', (req: Request, res: Response) => {
   res.send('');
 });
-
 // 記事新規投稿
 app.post('/post/create', (req: Request, res: Response) => {
   res.send('');
 });
-
 // 記事編集
 app.put('/post/update', (req: Request, res: Response) => {
   res.send('');
