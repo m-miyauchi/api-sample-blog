973d6dbf59c5dc82fac9fee132a905a5f86b3d98
MMiyauchi <masaki@miyauchi.me>
2020-10-26 15:31:28 +0900

    Feature/api endpoint (#2)
    * f
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,38 @@ import morgan from 'morgan';
 import { SERVER_PORT } from './constants/server_port';
 
 const app: Express = express();
-
 app.use(morgan('dev'));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('API for sample blog app.');
+});
+// ログイン
+app.put('/login', (req: Request, res: Response) => {
+  res.send('');
+});
+// ログアウト
+app.put('/logout', (req: Request, res: Response) => {
+  res.send('');
+});
+// 記事一覧取得
+app.get('/article', (req: Request, res: Response) => {
+  res.send('');
+});
+// 記事詳細取得
+app.get('/article/:id', (req: Request, res: Response) => {
+  res.send('');
+});
+// 記事新規投稿
+app.post('/article/create', (req: Request, res: Response) => {
+  res.send('');
+});
+// 記事編集
+app.put('/article/update/:id', (req: Request, res: Response) => {
+  res.send('');
+});
+// 記事削除
+app.delete('/article/deleter/:id', (req: Request, res: Response) => {
+  res.send('');
 });
 
 app.listen(SERVER_PORT, () => {
