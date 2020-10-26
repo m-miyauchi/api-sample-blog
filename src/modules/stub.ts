4fcfa72d8f7eb3eeff0f40d39fecb72c234b8bb1
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-26 18:56:42 +0900

    f

--- src/modules/stub.ts
+++ src/modules/stub.ts
@@ -1,24 +1,30 @@
-import faker, { fake } from 'faker';
+import faker from 'faker';
 import dayjs from 'dayjs';
 import { PutLoginResponse } from '../types/api/put_login_response';
 import { PutLogoutResponse} from '../types/api/put_logout_response';
 import { GetArticlesRespose } from '../types/api/get_articles_response';
+import { GetArticleResponse } from '../types/api/get_article_response';
+import { Article } from '../types/article';
 import { ArticleSummary } from '../types/article_summary';
 
 export class Stub {
-
+    private memberId: number = 1276;
+    private memberName: string ='Charlotte De Witte ';
     private articles: ArticleSummary[];
-
+    private articleCreatedAt: string;
+    private articleUpdatedAt: string;
     constructor() {
-        this.articles = this.genArticles();
+        this.articles = this.genArticles;
+        this.articleCreatedAt = dayjs().subtract(2, 'week').toString();
+        this.articleUpdatedAt =  dayjs().subtract(1, 'day').toString();
     }
 
     public putLogin(): PutLoginResponse {
         return {
             isSuccessLogin: true,
             member: {
-                id: 11234,
-                name: 'Charlotte De Witte ',
+                id: this.memberId,
+                name: this.memberName,
                 email: 'charlotte@de.witte'
             }
         }
@@ -32,17 +38,14 @@ export class Stub {
 
     public getArticles(): GetArticlesRespose {
         return {
-            articles: [
-                {
-                    title: '',
-                }
-            ],
+            articles: this.articles
         }
-
     }
 
-    public getArticle() {
-
+    public getArticle(): GetArticleResponse {
+        return {
+            article: this.genArticle,
+        }
     }
 
     public postArticle() {
@@ -57,16 +60,29 @@ export class Stub {
         
     }
 
-    private genArticles():ArticleSummary[] {
+    private get genArticles():ArticleSummary[] {
         const result: ArticleSummary[] = [];
         for (let i = 0; i < 10; i++) {
             const articleSummary: ArticleSummary = {
                 title: faker.lorem.words(),
-                summary: faker.lorem.paragraphs(),
+                summary: faker.lorem.sentences(),
                 updatedAt: dayjs().subtract(i, 'day').toString()
             };
             result.push(articleSummary);
         }
         return result;
+    }
+
+    private get genArticle():Article {
+        return {
+            title: faker.lorem.words(),
+            body: faker.lorem.sentences(), 
+            author: {
+                memberId: this.memberId,
+                name: this.memberName,
+            },
+            createdAt: this.articleCreatedAt,
+            updatedAt: this.articleUpdatedAt,
+        };
     }
 }
--- src/types/article_author.ts
+++ src/types/article_author.ts
@@ -1,5 +1,5 @@
 // 記事の投稿者情報
 export type ArticleAuthor =  {
-    userId: number // メンバーのシステム管理ID
+    memberId: number // メンバーのシステム管理ID
     name: string // メンバーのハンドルネーム
 }
