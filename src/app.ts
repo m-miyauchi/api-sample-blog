49f9e446d313fd1630d081bc0e03842ca2b907a4
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-26 16:09:24 +0900

    f

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, Express } from 'express';
 import morgan from 'morgan';
 import { SERVER_PORT } from './constants/server_port';
+import createDBConnection from './modules/create_db_connection';
 
 const app: Express = express();
+createDBConnection();
 
 app.use(morgan('dev'));
 
@@ -10,6 +12,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(SERVER_PORT, () => {
+app.listen(SERVER_PORT, async () => {
   console.log(`Server Started: http://127.0.0.1:${SERVER_PORT}`);
 });
