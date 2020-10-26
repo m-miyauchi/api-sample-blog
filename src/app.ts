606eaddbe17a60b6942ca4a6b124cd06fdaf6daf
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-26 15:07:29 +0900

    f

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,37 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+// ログイン
+app.put('/login', (req: Request, res: Response) => {
+  res.send('');
+});
+
+// ログアウト
+app.put('/logout', (req: Request, res: Response) => {
+  res.send('');
+});
+
+// 記事一覧取得
+app.get('/post', (req: Request, res: Response) => {
+  res.send('');
+});
+
+// 記事詳細取得
+app.get('/post', (req: Request, res: Response) => {
+  res.send('');
+});
+
+// 記事新規投稿
+app.post('/post/create', (req: Request, res: Response) => {
+  res.send('');
+});
+
+// 記事編集
+app.put('/post/update', (req: Request, res: Response) => {
+  res.send('');
+});
+
+
 app.listen(SERVER_PORT, () => {
   console.log(`Server Started: http://127.0.0.1:${SERVER_PORT}`);
 });
