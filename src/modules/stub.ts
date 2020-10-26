ac6ac17cce56a7d160a67641e908fbea35cf1191
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-26 18:29:47 +0900

    f

--- src/modules/stub.ts
+++ src/modules/stub.ts
@@ -1,14 +1,43 @@
+import faker, { fake } from 'faker';
+import dayjs from 'dayjs';
+import { PutLoginResponse } from '../types/api/put_login_response';
+import { PutLogoutResponse} from '../types/api/put_logout_response';
+import { GetArticlesRespose } from '../types/api/get_articles_response';
+import { ArticleSummary } from '../types/article_summary';
+
 export class Stub {
 
-    public putLogin() {
+    private articles: ArticleSummary[];
 
+    constructor() {
+        this.articles = this.genArticles();
     }
 
-    public putLogout() {
-
+    public putLogin(): PutLoginResponse {
+        return {
+            isSuccessLogin: true,
+            member: {
+                id: 11234,
+                name: 'Charlotte De Witte ',
+                email: 'charlotte@de.witte'
+            }
+        }
     }
 
-    public getArticles() {
+    public putLogout(): PutLogoutResponse {
+        return {
+            success: true,
+        }
+    }
+
+    public getArticles(): GetArticlesRespose {
+        return {
+            articles: [
+                {
+                    title: '',
+                }
+            ],
+        }
 
     }
 
@@ -26,5 +55,18 @@ export class Stub {
     
     public deleteArticle() {
         
+    }
+
+    private genArticles():ArticleSummary[] {
+        const result: ArticleSummary[] = [];
+        for (let i = 0; i < 10; i++) {
+            const articleSummary: ArticleSummary = {
+                title: faker.lorem.words(),
+                summary: faker.lorem.paragraphs(),
+                updatedAt: dayjs().subtract(i, 'day').toString()
+            };
+            result.push(articleSummary);
+        }
+        return result;
     }
 }
