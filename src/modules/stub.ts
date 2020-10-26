d2c13d7be485d9283b7a6a7952cdfc2a66cbcf09
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-26 17:22:59 +0900

    f

---
+++ src/modules/stub.ts
@@ -0,0 +1,30 @@
+export class Stub {
+
+    public putLogin() {
+
+    }
+
+    public putLogout() {
+
+    }
+
+    public getArticles() {
+
+    }
+
+    public getArticle() {
+
+    }
+
+    public postArticle() {
+
+    }
+    
+    public putArticle() {
+
+    }
+    
+    public deleteArticle() {
+        
+    }
+}
