36aed4b30b5dce795751ce753b333146b6c5a5a4
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-27 20:40:29 +0900

    f

--- src/types/api/put_article_request_params.ts
+++ src/types/api/put_article_request_params.ts
@@ -1,11 +1,9 @@
-
 // POST /article のリクエスト時パラメータ
 export type PutArticleRequestParams = {
-    memberId: number; // メンバーのシステム管理ID
-    article: {
-      id: number; // 記事のID
-      title: string; // 表題
-      body: string; // 本文
-    };
+  memberId: number; // メンバーのシステム管理ID
+  article: {
+    id: number; // 記事のID
+    title: string; // 表題
+    body: string; // 本文
   };
-  
+};
