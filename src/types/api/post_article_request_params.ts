4d9f953431d18122424d90d8104e687239bd4f25
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-27 21:09:43 +0900

    f

--- src/types/api/post_article_request_params.ts
+++ src/types/api/post_article_request_params.ts
@@ -1,6 +1,5 @@
 // POST /article のリクエスト時パラメータ
 export type PostArticleRequestParams = {
-  memberId: number; // メンバーのシステム管理ID
   article: {
     title: string; // 表題
     body: string; // 本文
--- src/types/api/put_article_request_params.ts
+++ src/types/api/put_article_request_params.ts
@@ -1,6 +1,5 @@
 // POST /article のリクエスト時パラメータ
 export type PutArticleRequestParams = {
-  memberId: number; // メンバーのシステム管理ID
   article: {
     id: number; // 記事のID
     title: string; // 表題
