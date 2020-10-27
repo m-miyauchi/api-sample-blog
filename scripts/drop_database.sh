20ec5f9eb98f941e46b86047d3a92ff73534e92f
MMiyauchi <masaki@miyauchi.me>
2020-10-27 14:04:12 +0900

    f (#7)

--- scripts/drop_database.sh
+++ scripts/drop_database.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker-compose up -d &&
 sleep 5;
-docker-compose exec db-test psql -U postgres -c "DROP DATABASE commmit;"
+docker-compose exec db-test psql -U postgres -c "DROP DATABASE blog;"
 docker-compose down
