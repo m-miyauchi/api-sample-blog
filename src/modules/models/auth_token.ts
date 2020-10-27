dc63891713223eef82bef36de6192ac802f0be6f
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-27 16:59:33 +0900

    f

--- src/modules/models/auth_token.ts
+++ src/modules/models/auth_token.ts
@@ -1,10 +1,10 @@
+import dayjs from 'dayjs';
 import { getRepository, Connection, Repository } from 'typeorm';
-import { Member as MemberEntity } from '../../entity/member';
 import { AuthToken as AuthTokenEntity } from '../../entity/auth_token';
 
 export default class AuthToken {
   private connectionName = 'app';
-  private repository: Repository<MemberEntity>;
+  private repository: Repository<AuthTokenEntity>;
 
   constructor(connection?: Connection) {
     if (connection !== undefined) {
@@ -15,12 +15,16 @@ export default class AuthToken {
     this.repository = getRepository(AuthTokenEntity, this.connectionName);
   }
 
-  private createToken(mebmerId: number): Promise<AuthTokenEntity> {
+  public async createToken(mebmerId: number): Promise<AuthTokenEntity> {
     try {
-      const repository: Repository<AuthTokenEntity> = await getRepository(
-        AuthTokenEntity,
-        this.connectionName
-      );
-    } catch (error) {}
+      const token = await this.repository.save({
+        member_id: mebmerId,
+        token: `T_${mebmerId}_${dayjs().toISOString()}`,
+        expired_at: dayjs().add(4, 'week'),
+      });
+      return token;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
