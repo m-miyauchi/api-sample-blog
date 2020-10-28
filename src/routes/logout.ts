11a04de3f567a962309902cf1cd30b93ad0392b4
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-28 13:26:07 +0900

    f

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -1,12 +1,20 @@
-import express, { Request, Response } from 'express';
+import express, { Response } from 'express';
 import MemberModel from '../modules/models/member';
+import { Req } from '../interfaces/request_headers';
 const router = express.Router();
 
 // ログアウト
-router.put('/', async (req: Request, res: Response) => {
+router.put('/', async (req: Req, res: Response) => {
   const memberModel = new MemberModel();
-  memberModel.logout(req.headers.authorization);
-  res.status(204).end();
+  try {
+    const t = await memberModel.logout(req.headers.token);
+    if (t !== void 0) {
+      res.status(204).end();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  res.status(400).end();
 });
 
 export default router;
