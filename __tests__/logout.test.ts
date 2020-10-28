2ef51ca2e0c68cc409c1af6fef39f3e5bd40e40d
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-28 17:30:11 +0900

    f

--- __tests__/logout.test.ts
+++ __tests__/logout.test.ts
@@ -3,10 +3,15 @@ import { PutLogoutResponse } from '../src/types/api/put_logout_response';
 // constants
 import { SERVER_PORT } from '../src/constants/server_port';
 
+//  ログインのテストのあとに
 describe('ログアウト', () => {
   test('logout', async () => {
     const r: AxiosResponse<PutLogoutResponse> = await axios.post(
-      `http://127.0.0.1:${SERVER_PORT}/logout`
+      `http://127.0.0.1:${SERVER_PORT}/logout`, {}, {
+        headers: {
+          auth: ''
+        }
+      }
     );
     console.log(r);
     expect(r.data.success).toBeTruthy();
