ae47cfc6aa013d3e39145c4282188ed8283d77d2
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-28 11:36:52 +0900

    f

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import helmet from 'helmet';
 import { SERVER_PORT } from './constants/server_port';
 import createDBConnection from './modules/create_db_connection';
 import Stub from './modules/stub';
+// routes
+import loginRoutes  from './routes/login';
+import articleRoutes from './routes/article';
 
 function main() {
   // setup
@@ -11,40 +14,14 @@ function main() {
   const app: Express = express();
   app.use(morgan('dev'));
   app.use(helmet());
-  const stub = new Stub();
+
+  app.use('login', loginRoutes);
+  app.use('article', articleRoutes);
 
   // routes
   app.get('/', (req: Request, res: Response) => {
     res.send('API for sample blog app.');
   });
-
-  // 記事 一覧取得
-  app.get('/articles', (req: Request, res: Response) => {
-    res.send(stub.getArticles());
-  });
-  // 記事 詳細取得
-  app.get('/article/:id', (req: Request, res: Response) => {
-    res.send(stub.getArticle());
-  });
-  // 記事 新規投稿
-  app.post(
-    '/article',
-    (req: Request<PostArticleRequestParams>, res: Response) => {
-      res.status(204).end();
-    }
-  );
-  // 記事 編集
-  app.put(
-    '/article',
-    (req: Request<PutArticleRequestParams>, res: Response) => {
-      res.status(204).end();
-    }
-  );
-  // 記事 削除
-  app.delete('/article/:id', (req: Request, res: Response) => {
-    res.status(204).end();
-  });
-
   app.listen(SERVER_PORT, async () => {
     console.log(`Server Started: http://127.0.0.1:${SERVER_PORT}`);
   });
