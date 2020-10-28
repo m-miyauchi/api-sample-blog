26caa4500200c039bb20f68ec64db52e131e4c81
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-28 13:04:23 +0900

    f

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -1,9 +1,5 @@
 import express, { Request, Response } from 'express';
-import { PutLoginParams } from '../types/api/put_login_params';
-import Stub from '../modules/stub';
-
 const router = express.Router();
-const stub = new Stub();
 
 // ログアウト
 router.put('/', (req: Request, res: Response) => {
