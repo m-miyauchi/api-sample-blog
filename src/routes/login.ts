f4731022d0497614e96f428beb3e80cf486794ea
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-28 14:51:43 +0900

    f

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -6,20 +6,23 @@ import { PutLoginResponse } from '../types/api/put_login_response';
 const router = express.Router();
 
 // ログイン
-router.put('/', async (req: Request<PutLoginParams>, res: Response) => {
-  const memberModel = new MemberModel();
-  let r: PutLoginResponse;
+router.put(
+  '/',
+  async (req: Request<PutLoginParams>, res: Response<PutLoginResponse>) => {
+    const memberModel = new MemberModel();
+    let r: PutLoginResponse;
 
-  try {
-    r = await memberModel.login(req.body);
-    if (r !== void 0) {
-      res.send(r);
+    try {
+      r = await memberModel.login(req.body);
+      if (r !== void 0) {
+        res.send(r);
+      }
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
 
-  res.send(r);
-});
+    res.send(r);
+  }
+);
 
 export default router;
