8b805cdbb7ca69302d4ebaa2b8bcc2e57857bade
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-29 11:04:33 +0900

    f

--- __tests__/login.test.ts
+++ __tests__/login.test.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 import { PutLoginParams } from '../src/types/api/put_login_params';
 import { PutLoginResponse } from '../src/types/api/put_login_response';
+import { PutLogoutResponse } from '../src/types/api/put_logout_response';
 // constants
 import { SERVER_PORT } from '../src/constants/server_port';
 
-describe('ログイン', () => {
+describe('ログイン・ログアウト', () => {
   test('login', async () => {
     const params: PutLoginParams = {
       email: 'charlotte@de.witte',
@@ -17,5 +18,19 @@ describe('ログイン', () => {
     );
     console.log(r);
     expect(r.data.isSuccessLogin).toBeTruthy();
+  });
+
+  test('logout', async () => {
+    const r: AxiosResponse<PutLogoutResponse> = await axios.post(
+      `http://127.0.0.1:${SERVER_PORT}/logout`,
+      {},
+      {
+        headers: {
+          auth: '',
+        },
+      }
+    );
+    console.log(r);
+    expect(r.data.success).toBeTruthy();
   });
 });
