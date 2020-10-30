5af10fcff7e3a6b6f95f2982f252f5ad39f03919
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-10-30 11:54:50 +0900

    f

--- src/modules/models/aritcle.ts
+++ src/modules/models/aritcle.ts
@@ -71,7 +71,6 @@ export default class Article {
       const t = await authTokenModel.findEnableToken(tokenCode);
       a = await this.repository.findOne(params.article.id);
 
-      console.log(a.author_member_id, t.member_id);
       if (a !== void 0) {
         if (a.author_member_id === t.member_id) {
           a.title = params.article.title;
