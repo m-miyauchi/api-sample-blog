c42e713d0f29d911783d9ed75eb8a0a1dd9f6ae6
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-12-25 09:54:38 +0900

    f

--- src/app.ts
+++ src/app.ts
@@ -18,16 +18,22 @@ async function main() {
   app.use(helmet());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  if (app.get('env') === 'development') {
-    app.use(function (req, res, next) {
+
+  app.use(function (req, res, next) {
+    res.header(
+      'Access-Control-Allow-Methods',
+      'POST, PUT, DELETE, GET, OPTIONS'
+    );
+    // クロスオリジン許可
+    if (app.get('env') === 'development') {
       res.header('Access-Control-Allow-Origin', '*');
       res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
       );
-      next();
-    });
-  }
+    }
+    next();
+  });
 
   // routes
   app.use('/login', loginRoute);
