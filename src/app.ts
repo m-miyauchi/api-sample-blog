483ac34cb1829e30deb91d12d504c8460b8ee71d
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2020-12-25 09:56:32 +0900

    f

--- src/app.ts
+++ src/app.ts
@@ -18,12 +18,13 @@ async function main() {
   app.use(helmet());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
+  // HTTP header settings
   app.use(function (_req, res, next) {
     res.header(
       'Access-Control-Allow-Methods',
       'POST, PUT, DELETE, GET, OPTIONS'
     );
-    // クロスオリジン許可
+    // Arrow CORS
     if (app.get('env') === 'development') {
       res.header('Access-Control-Allow-Origin', '*');
       res.header(
