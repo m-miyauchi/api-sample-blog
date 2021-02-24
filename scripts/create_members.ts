341a385deebf6754e7641a8c0c360a38a3540044
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2021-02-24 14:43:26 +0900

    f

--- scripts/create_members.ts
+++ scripts/create_members.ts
@@ -2,18 +2,25 @@ import faker from 'faker';
 import MemberModel from '../src/modules/models/member';
 import createDbConnection from '../src/modules/create_db_connection';
 
+const NUM = 500;
+
 async function createMember() {
   const member = new MemberModel();
-
   const name = `${faker.name.firstName()} ${faker.name.lastName()}`;
-  const email = `${faker.internet.email}`;
-  const password = `${faker.internet.password}`;
+  const email = `${faker.internet.email()}`;
+  const password = `${faker.internet.password()}`;
+
+  console.log(name, email, password);
 
   await member.register(name, email, password);
 }
 
 async function main() {
   await createDbConnection();
-  await createMember();
+
+  for (let index = 0; index < NUM; index++) {
+    await createMember();
+  }
+  process.exit(0);
 }
 main();
