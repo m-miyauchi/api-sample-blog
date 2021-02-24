619ad5abb2e52c97b422e6855335ba55342eb0b1
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2021-02-24 15:22:36 +0900

    f

--- scripts/create_member.ts
+++ scripts/create_member.ts
@@ -4,7 +4,13 @@ import createDbConnection from '../src/modules/create_db_connection';
 async function main() {
   await createDbConnection();
   const member = new MemberModel();
-  // パラメータを任意に変えて実行
-  await member.register('Charlotte', 'charlotte@de.witte', 'password');
+  try {
+    // パラメータを任意に変えて実行
+    await member.register('Charlotte', 'charlotte@de.witte', 'password');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  process.exit(9);
 }
 main();
