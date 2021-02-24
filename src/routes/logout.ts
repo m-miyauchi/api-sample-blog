2fb518bcd0fb96826e1479793bfdf52c6797d926
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2021-02-24 13:39:36 +0900

    f

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -8,18 +8,18 @@ const router = express.Router();
 // ログアウト
 router.put('/', async (req: Request, res: Response) => {
   const memberModel = new MemberModel();
-  const basicResponse: PutLogoutResponse = { success: false }
+  const basicResponse: PutLogoutResponse = { success: false };
   try {
     // @ts-ignore
     const t = await memberModel.logout(req.headers.auth);
     if (t !== void 0) {
-      const r: PutLogoutResponse = { ...basicResponse, success: true }
+      const r: PutLogoutResponse = { ...basicResponse, success: true };
       res.status(200).send(r).end();
       return 0;
     }
   } catch (error) {
     console.error(error);
-    const r: PutLogoutResponse = { ...basicResponse, error }
+    const r: PutLogoutResponse = { ...basicResponse, error };
     res.status(400).send(r).end();
   }
   res.status(400).end();
