a7464d1553b277d66a962439b19f31b4f0b343eb
Masaki Miyauchi <masaki.miyauchi@andpad.co.jp>
2021-02-25 12:51:26 +0900

    f

--- src/types/api/get_articles_response.ts
+++ src/types/api/get_articles_response.ts
@@ -1,6 +1,8 @@
 import { ArticleSummary } from '../article_summary';
+import { Page } from './page';
 
 // GET /article の返し値
 export type GetArticlesRespose = {
   articles: ArticleSummary[];
+  page: Page;
 };
---
+++ src/types/api/page.ts
@@ -0,0 +1,6 @@
+export type Page = {
+    count: number;
+    limit: number;
+    currentPage: number;
+    totalPage: number;
+}
